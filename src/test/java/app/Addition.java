54e96cddb83bc273965a608359d09bb410de5911
hemali <hemali.bhanderi@gmail.com>
2020-10-16 13:07:09 -0700

    first commit

---
+++ src/test/java/app/Addition.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		System.out.println( 3+ 4);
+
+	}
+
+}
---
+++ src/test/java/app/Multiplication.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
